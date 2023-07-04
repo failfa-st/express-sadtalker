3ea5ebabb8a2e59e7588ab358b313eb2909443c4
Tim Pietrusky <timpietrusky@gmail.com>
2023-07-04 17:24:25 +0200

    feat: updated sadtalker to latest version

--- webui.bat
+++ webui.bat
@@ -10,7 +10,7 @@ call .\venv\Scripts\activate.bat
 set PYTHON="venv\Scripts\Python.exe"
 echo venv %PYTHON%
 
-%PYTHON% launcher.py
+%PYTHON% Launcher.py
 
 echo.
 echo Launch unsuccessful. Exiting.
